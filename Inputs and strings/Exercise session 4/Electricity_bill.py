bd6b2881a8146a07224ac6ab5a7fc22f9297f623
iscode34 <mohammad.istahad_ti25@nusaputra.ac.id>
2026-03-20 09:09:51 +0700

    Update Electricity_bill.py

--- Inputs and strings/Exercise session 4/Electricity_bill.py
+++ Inputs and strings/Exercise session 4/Electricity_bill.py
@@ -1,1 +1,12 @@
+units = float(input("Enter electricity usage (kWh): "))
 
+if units <= 100:
+    price = 0.10
+elif units <= 300:
+    price = 0.15
+else:
+    price = 0.20
+
+total = units * price
+
+print("Total bill: $", total)
